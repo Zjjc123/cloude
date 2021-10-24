c6510b20d90e87d2b767ed07d6c8363c1e40a8b9
Jason Zhang <zjjc123@hotmail.com>
2021-10-23 23:35:22 -0700

    Basic Online API Python Interpreter

--- src/pages/compiler.js
+++ src/pages/compiler.js
@@ -10,7 +10,7 @@ function Compiler() {
         stdin: "",
     };
 
-    const submit = async (e) => {
+    const submit = (e) => {
         console.log("input: ", input);
         e.preventDefault();
         fetch("https://judge0-ce.p.rapidapi.com/submissions", {
@@ -23,56 +23,55 @@ function Compiler() {
             },
             body: JSON.stringify(requestBody),
         })
+            .then((response) => response.json())
             .then((response) => {
                 parseResponse(response);
-                console.log(response);
             })
             .catch((err) => {
                 console.error(err);
             });
     };
-    const parseResponse = async (response) => {
-        const jsonResponse = await response.json();
-        let jsonGetSolution = {
-            status: { description: "Queue" },
-            stderr: null,
-            compile_output: null,
-        };
-        while (
-            jsonGetSolution.status.description !== "Accepted" &&
-            jsonGetSolution.stderr == null &&
-            jsonGetSolution.compile_output == null
-        ) {
-            console.log(jsonGetSolution);
-            if (jsonResponse.token) {
-                let url = `https://judge0-ce.p.rapidapi.com/submissions/${jsonResponse.token}?base64_encoded=true`;
-                const getSolution = await fetch(url, {
-                    method: "GET",
-                    headers: {
-                        "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
-                        "x-rapidapi-key":
-                            "2a3d3dfb60mshcff5d7b612c90abp146cf6jsnefb8b730db97",
-                        "content-type": "application/json",
-                    },
-                });
-                jsonGetSolution = await getSolution.json();
-            }
-        }
+    const parseResponse = (response) => {
+        console.log("response token: " + response.token);
+        if (response.token) {
+            let url = `https://judge0-ce.p.rapidapi.com/submissions/${response.token}?base64_encoded=true`;
+            fetch(url, {
+                method: "GET",
+                headers: {
+                    "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+                    "x-rapidapi-key":
+                        "2a3d3dfb60mshcff5d7b612c90abp146cf6jsnefb8b730db97",
+                    "content-type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    console.log("stdout: " + response.stdout);
+                    if (response.stdout) {
+                        const buf = Buffer.from(response.stdout, "base64");
+                        const outputString = buf.toString("utf8");
+                        setOutput(
+                            `Results :\n${outputString}\nExecution Time : ${response.time} Secs\nMemory used : ${response.memory} bytes`
+                        );
+                        console.log("output: " + output);
+                    } else if (response.stderr) {
+                        const buf = Buffer.from(response.stderr, "base64");
+                        const error = buf.toString("utf8");
+                        setOutput(`\n Error :${error}`);
+                    } else {
+                        const buf = Buffer.from(
+                            response.compile_output,
+                            "base64"
+                        );
+                        const compilation_error = buf.toString("utf8");
+                        setOutput(`\n Error :${compilation_error}`);
+                    }
 
-        if (jsonGetSolution.stdout) {
-            const buf = Buffer.from(jsonGetSolution.stdout, "base64");
-            const output = buf.toString("base64");
-            setOutput(
-                `Results :\n${output}\nExecution Time : ${jsonGetSolution.time} Secs\nMemory used : ${jsonGetSolution.memory} bytes`
-            );
-        } else if (jsonGetSolution.stderr) {
-            const buf = Buffer.from(jsonGetSolution.stderr, "base64");
-            const error = buf.toString("base64");
-            setOutput(`\n Error :${error}`);
-        } else {
-            const buf = Buffer.from(jsonGetSolution.compile_output, "base64");
-            const compilation_error = buf.toString("base64");
-            setOutput(`\n Error :${compilation_error}`);
+                    console.log(JSON.stringify(response));
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
         }
     };
 
@@ -104,7 +103,7 @@ function Compiler() {
                             <i className="fas fa-exclamation fa-fw fa-md"></i>{" "}
                             Output
                         </span>
-                        <textarea id="output">{output}</textarea>
+                        <textarea id="output" value={output}></textarea>
                     </div>
                 </div>
             </div>
